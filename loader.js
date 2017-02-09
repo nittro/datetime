f61c79f3bcd85da886e8f3dabb8ad4489119e7c9
danik <me@subsonic.cz>
2017-02-10 00:10:02 +0100

    Refactoring ahead of 2.0

--- loader.js
+++
@@ -1,5 +0,0 @@
-/* jahudka/minify compatible loader */
-/*! @include file="src/js/Utils/DateInterval.js" */
-/*! @include file="src/js/Utils/DateTime.js" */
-/*! @include file="src/js/Utils/DateTime.i18n.js" */
-/*! @include file="src/js/Utils/DateTime.en.js" */
